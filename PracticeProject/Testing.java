0e16cb7c78545ce600a7feb64cdf97e94d0adfde
Vijayakrishnamoorthy <vijaykrishna.psr@gmail.com>
2026-07-11 11:56:06 +0530

    My First Commit

---
+++ PracticeProject/Testing.java
@@ -0,0 +1,13 @@
+package firstPractice;
+
+import org.testng.annotations.Test;
+
+public class Testing {
+	
+	@Test
+	public void get() {
+		System.out.println("TestNG Installed Successfully");
+		
+	}
+
+}
---
+++ PracticeProject/src/test/java/firstPractice/Testing.java
@@ -0,0 +1,39 @@
+package firstPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testing {
+
+	@Test
+	public void launchChromeBrowser() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+
+	}
+
+	@Test(priority = 1)
+	public void launchFirefoxBrowser() {
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.flipkart.com/");
+	}
+
+	@Test
+	public void signUp() {
+		System.out.println("SignUp completed");
+	}
+
+	@Test(dependsOnMethods = "signUp", invocationCount = 3, invocationTimeOut = 10000, threadPoolSize = 3)
+	public void login() {
+		System.out.println("Logged in successfully");
+		
+	}
+
+}
